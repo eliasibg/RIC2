34f558c32a17c6ffe464a57d5751537ce2aae171
FAILMymy <mogotec@hotmail.com>
2019-05-09 17:19:08 -0500

    Primero Signed-off-by: FAILMymy <mogotec@hotmail.com>

---
+++ RIC/Controllers/RespuestasController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RIC.Views.Respuestas
+{
+    public class RespuestasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("VistaIndexRespuesta");
+        }
+
+        public IActionResult crear()
+        {
+            return View("VistaCrearRespuesta");
+        }
+
+        public IActionResult borrar()
+        {
+            return View("VistaBorrarRespuesta");
+        }
+
+        public IActionResult modificar()
+        {
+            return View("VistaModificarRespuesta");
+        }
+
+        public IActionResult buscar()
+        {
+            return View("VistaBuscarRespuesta");
+        }
+
+        public IActionResult listar()
+        {
+            return View("VistaListarRespuesta");
+        }
+
+    }
+}
---
+++ RIC/Models/Respuesta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RIC.Models
+{
+    public class Respuesta
+    {
+
+        private String strRespuesta { get; set; }
+        private String strMotivo { get; set; }
+        private String blEstatus { get; set; }
+
+    }
+}
